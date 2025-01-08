655d016e791c2ca0a45c0fd4fbe31fef7869f949
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-08 00:13:56 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Sherry is the champion";
